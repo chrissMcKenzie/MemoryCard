dc644d83a5dfdb7db0839327f7c63e8408fd7cdc
chrissMcKenzie <chrissMcKenzie.IT.Agence@gmail.com>
2022-03-11 22:31:13 +0100

    Architecture Global: Mise à jour de l'architecture, pour l'utilisation de phpUnit

--- V1/Controller/PatientController.php
+++
@@ -1,9 +0,0 @@
-<?php require_once './../Model/DatabaseModel.php';
-require_once './../Model/PatientClass.php';
-
-extract($_POST); var_dump($_POST);
-//$date = $_POST['date'];
-//result = addPatient($date, $consommation, $prix, 0);
-//header("location:UserView");
-
-include_once './../View/UserView.php';
--- V3/Controller/GraphController.php
+++
@@ -1,8 +0,0 @@
-<?php
-include_once('./model/DatabaseModel.php');
-include_once('./model/PatientClass.php');
-
-    if(isset($_GET['index'])) {		// l'URL complétée par ?page=3&index=yy fournit l'index Person souhaité
-        $index = $_GET['index'];
-            include("View/graphPatient.php");  // cette vue dispose de $person et de $filmList
-    }
